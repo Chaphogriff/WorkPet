c5fed9112e7367a1db309d038091fc2f434c13bb
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2023-03-12 13:49:30 -0400

    Ajout Test Button Listener et changement d'activité

--- app/src/main/java/com/workthrutheweak/workpet/MainActivity.java
+++ app/src/main/java/com/workthrutheweak/workpet/MainActivity.java
@@ -2,13 +2,31 @@ package com.workthrutheweak.workpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    ImageButton logo_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Récupérer les éléments du xml
+        logo_main = (ImageButton) findViewById(R.id.logo_workpet);
+
+        // Mettre en place les listeners
+
+        // Appuyer le bouton nous envoie vers un autre activité
+        logo_main.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, TestActivity.class))
+        );
+
     }
 }
---
+++ app/src/main/java/com/workthrutheweak/workpet/TestActivity.java
@@ -0,0 +1,17 @@
+package com.workthrutheweak.workpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class TestActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
